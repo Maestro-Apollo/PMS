b81b4d559360a4f3575bdc6c4a9502b89fdd0743
Rafi Mahafid <rafimahafid@gmail.com>
2022-04-05 00:16:46 +0600

    search

--- search-page.php
+++ search-page.php
@@ -36,7 +36,7 @@ $obj = new Property;
         width: 75%;
     }
     </style>
-
+    <!-- <script src="https://kit.fontawesome.com/0c7895b5e7.js" crossorigin="anonymous"></script> -->
 </head>
 
 <body class="bg-light">
@@ -45,54 +45,73 @@ $obj = new Property;
     <section>
         <div class="container bg-white">
 
-            <form action="" class="p-3">
+            <form action="./search-result.php" method="post" class="p-3">
                 <label for="" class="mt-3 font-weight-bold">Building Info</label>
-                <input type="text" name="building" class="form-control ">
-                <p class="mt-3 mb-0 font-weight-bold">Facilities</p>
+                <input type="text" name="building" class="form-control " required>
+                <div class="d-block">
+                    <div class="float-left">
+                        <p class="mt-3 mb-0 font-weight-bold ">Facilities <i class="fas fa-arrow-right"></i></p>
+
+                    </div>
+                    <div class="float-right">
+                        <p class="mt-3 mb-0 font-weight-bold ">Include</p>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" name="include1" value="Yes" id="include1"
+                                checked>
+                            <label class="form-check-label" for="include1">Yes</label>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <br>
+                <br>
                 <p class="mb-0">Gross Area</p>
 
                 <div class="row">
                     <div class="col-6">
-                        <input type="text" name="gross1" placeholder="From" class="form-control ">
+                        <input type="number" name="gross1" placeholder="From" class="form-control " autocomplete="off">
 
                     </div>
                     <div class="col-6">
-                        <input type="text" name="gross2" placeholder="To" class="form-control ">
+                        <input type="number" name="gross2" placeholder="To" class="form-control " autocomplete="off">
 
                     </div>
                 </div>
                 <p class="mt-3 mb-0">Sailable Area</p>
                 <div class="row">
                     <div class="col-6">
-                        <input type="text" name="sailable1" placeholder="From" class="form-control ">
+                        <input type="number" name="sailable1" placeholder="From" class="form-control "
+                            autocomplete="off">
 
                     </div>
                     <div class="col-6">
-                        <input type="text" name="sailable2" placeholder="To" class="form-control ">
+                        <input type="number" name="sailable2" placeholder="To" class="form-control " autocomplete="off">
 
                     </div>
                 </div>
                 <p class="mt-3 mb-0">Price Range</p>
                 <div class="row">
                     <div class="col-6">
-                        <input type="text" name="price1" placeholder="From" class="form-control ">
+                        <input type="number" name="price1" placeholder="From" class="form-control " autocomplete="off">
 
                     </div>
                     <div class="col-6">
-                        <input type="text" name="price2" placeholder="To" class="form-control ">
+                        <input type="number" name="price2" placeholder="To" class="form-control " autocomplete="off">
 
                     </div>
                 </div>
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Cargo" value="Yes" id="Cargo">
+                            <input type="checkbox" class="form-check-input" name="Cargo" value="Yes" id="Cargo" checked>
                             <label class="form-check-label" for="Cargo">Cargo Lift</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Customer" value="Yes" id="Customer">
+                            <input type="checkbox" class="form-check-input" name="Customer" value="Yes" id="Customer"
+                                checked>
                             <label class="form-check-label" for="Customer">Customer Lift</label>
                         </div>
                     </div>
@@ -100,13 +119,14 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="hours" value="Yes" id="hours">
+                            <input type="checkbox" class="form-check-input" name="hours" value="Yes" id="hours" checked>
                             <label class="form-check-label" for="hours">24 hours</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Windows" value="Yes" id="Windows">
+                            <input type="checkbox" class="form-check-input" name="Windows" value="Yes" id="Windows"
+                                checked>
                             <label class="form-check-label" for="Windows">Windows</label>
                         </div>
                     </div>
@@ -114,13 +134,15 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Lavatory" value="Yes" id="Lavatory">
+                            <input type="checkbox" class="form-check-input" name="Lavatory" value="Yes" id="Lavatory"
+                                checked>
                             <label class="form-check-label" for="Lavatory">Lavatory</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Shower" value="Yes" id="Shower">
+                            <input type="checkbox" class="form-check-input" name="Shower" value="Yes" id="Shower"
+                                checked>
                             <label class="form-check-label" for="Shower">Shower</label>
                         </div>
                     </div>
@@ -128,13 +150,13 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Sink" value="Yes" id="Sink">
+                            <input type="checkbox" class="form-check-input" name="Sink" value="Yes" id="Sink" checked>
                             <label class="form-check-label" for="Sink">Sink</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" name="Door" value="Yes" class="form-check-input" id="Door">
+                            <input type="checkbox" name="Door" value="Yes" class="form-check-input" id="Door" checked>
                             <label class="form-check-label" for="Door">Wide Door</label>
                         </div>
                     </div>
@@ -142,14 +164,14 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Wall" value="Yes" id="Wall">
+                            <input type="checkbox" class="form-check-input" name="Wall" value="Yes" id="Wall" checked>
                             <label class="form-check-label" for="Wall">Bricked Wall</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="SeparateRoom" name="SeparateRoom"
-                                value="Yes">
+                                value="Yes" checked>
                             <label class="form-check-label" for="SeparateRoom">Separate room</label>
                         </div>
                     </div>
@@ -157,29 +179,47 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Keys" value="Yes" id="Keys">
+                            <input type="checkbox" class="form-check-input" name="Keys" value="Yes" id="Keys" checked>
                             <label class="form-check-label" for="Keys">Electronic Keys</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" name="Wifi" value="Yes" class="form-check-input" id="Wifi">
+                            <input type="checkbox" name="Wifi" value="Yes" class="form-check-input" id="Wifi" checked>
                             <label class="form-check-label" for="Wifi">Wifi</label>
                         </div>
                     </div>
                 </div>
-                <p class="mt-3 mb-0 font-weight-bold">Types</p>
+                <div class="d-block">
+                    <div class="float-left">
+                        <p class="mt-3 mb-0 font-weight-bold ">Types <i class="fas fa-arrow-right"></i></p>
+
+                    </div>
+                    <div class="float-right">
+                        <p class="mt-3 mb-0 font-weight-bold ">Include</p>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" name="include2" value="Yes" id="include2"
+                                checked>
+                            <label class="form-check-label" for="include2">Yes</label>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <br>
+                <br>
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="individual" name="individual"
-                                value="Yes">
+                                value="Yes" checked>
                             <label class="form-check-label" for="individual">Individual</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Separate" value="Yes" id="seprate">
+                            <input type="checkbox" class="form-check-input" name="Separate" value="Yes" id="seprate"
+                                checked>
                             <label class="form-check-label" for="seprate">Separate</label>
                         </div>
                     </div>
@@ -187,13 +227,14 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Studio" value="Yes" id="studio">
+                            <input type="checkbox" class="form-check-input" name="Studio" value="Yes" id="studio"
+                                checked>
                             <label class="form-check-label" for="studio">Studio</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Yoga" value="Yes" id="yoga">
+                            <input type="checkbox" class="form-check-input" name="Yoga" value="Yes" id="yoga" checked>
                             <label class="form-check-label" for="yoga">Yoga</label>
                         </div>
                     </div>
@@ -201,13 +242,15 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Class2" value="Yes" id="class2">
+                            <input type="checkbox" class="form-check-input" name="Class2" value="Yes" id="class2"
+                                checked>
                             <label class="form-check-label" for="class2">Class</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="overnight" name="Overnight" value="Yes">
+                            <input type="checkbox" class="form-check-input" id="overnight" name="Overnight" value="Yes"
+                                checked>
                             <label class="form-check-label" for="overnight">Overnight</label>
                         </div>
                     </div>
@@ -216,13 +259,14 @@ $obj = new Property;
                     <div class="col-6">
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="warehouse_office" name="Warehouse"
-                                value="Yes">
+                                value="Yes" checked>
                             <label class="form-check-label" for="warehouse_office">Warehouse office</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Beauty" value="Yes" id="beauty">
+                            <input type="checkbox" class="form-check-input" name="Beauty" value="Yes" id="beauty"
+                                checked>
                             <label class="form-check-label" for="beauty">Beauty</label>
                         </div>
                     </div>
@@ -230,13 +274,14 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="upstair_shop" name="Shop" value="Yes">
+                            <input type="checkbox" class="form-check-input" id="upstair_shop" name="Shop" value="Yes"
+                                checked>
                             <label class="form-check-label" for="upstair_shop">Upstairs shop</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Band" value="Yes" id="band">
+                            <input type="checkbox" class="form-check-input" name="Band" value="Yes" id="band" checked>
                             <label class="form-check-label" for="band">Band</label>
                         </div>
                     </div>
@@ -245,13 +290,13 @@ $obj = new Property;
                     <div class="col-6">
                         <div class="form-group form-check">
                             <input type="checkbox" class="form-check-input" id="recording_room" name="Recording"
-                                value="Yes">
+                                value="Yes" checked>
                             <label class="form-check-label" for="recording_room">Recording room</label>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Piano" value="Yes" id="piano">
+                            <input type="checkbox" class="form-check-input" name="Piano" value="Yes" id="piano" checked>
                             <label class="form-check-label" for="piano">Piano</label>
                         </div>
                     </div>
@@ -259,7 +304,8 @@ $obj = new Property;
                 <div class="row mt-3">
                     <div class="col-6">
                         <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" name="Painting" value="Yes" id="painting">
+                            <input type="checkbox" class="form-check-input" name="Painting" value="Yes" id="painting"
+                                checked>
                             <label class="form-check-label" for="painting">Painting</label>
                         </div>
                     </div>
---
+++ share.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <meta property="og:url"
+        content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" /> -->
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="<?php echo $_GET['img']; ?>" />
+    <meta property="og:description" content="How much does culture influence creative thinking?" />
+    <meta property="og:image" content="files/<?php echo $_GET['img']; ?>" />
+</head>
+
+<body>
+
+    <a href="files/<?php echo $_GET['img']; ?>"><img src="files/<?php echo $_GET['img']; ?>" alt=""></a>
+
+</body>
+
+</html>
